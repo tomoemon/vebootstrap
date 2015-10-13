b2271b7fc61be5bf6b83b12c91ac40ea84037a3b
tomoemon <bach48+github_tomoemon@gmail.com>
2015-10-13 21:45:37 +0900

    fix help

--- vebootstrap/create.py
+++ vebootstrap/create.py
@@ -51,7 +51,7 @@ def create_virtualenv_bootstrap_script(script_path, after_install_script_file):
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog="python -m vebootstrap.create")
     parser.add_argument("--output", default="virtualenv-bootstrap.py")
     parser.add_argument("--after-install",  default="")
     args = parser.parse_args()
