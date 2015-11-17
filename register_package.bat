b64ee8029ef496870a31dc06a63c5721089b4a68
tomoemon <bach48+github_tomoemon@gmail.com>
2015-11-17 17:00:47 +0900

    insert pause command before register/upload to production

--- register_package.bat
+++ register_package.bat
@@ -1,3 +1,7 @@
+echo テスト用 PyPI へ登録します
 python setup.py register -r https://testpypi.python.org/pypi
+echo 次に本番 PyPI へ登録します
+pause
 python setup.py register
+echo 登録完了しました
 
--- upload_sdist.bat
+++ upload_sdist.bat
@@ -1,3 +1,6 @@
+echo テスト用 PyPI へアップロードします
 python setup.py sdist upload -r https://testpypi.python.org/pypi
+echo 次に本番 PyPI へアップロードします
+pause
 python setup.py sdist upload
-
+echo 本番へのアップロード完了しました
